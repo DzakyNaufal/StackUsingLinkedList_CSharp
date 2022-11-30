4c8b8780d15dcedaa7f5e069ad23dc7c9fba7b05
DzakyNaufal <dzakynmulna@gmail.com>
2022-11-30 16:09:59 +0700

    Add project files.

---
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StackUsingLinkedList_CSharp
+{
+    class Node
+    {
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
